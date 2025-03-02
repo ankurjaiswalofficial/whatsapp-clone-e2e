32fcc354b129841d048328a6aa9fe8d4b63921c2
Ankur Jaiswal <ankurjaiswalofficial@gmail.com>
2025-03-02 19:50:01 +0530

    Removed terms and conditions page and improved email url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           <li className="mb-2">
             Want access to the repository please contact me at&nbsp;
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              <Link target="_blank" href={"https://mail.google.com/mail/?view=cm&fs=1&to=&su=Request%20for%20Codebase%20Access%20-%20(Whatsapp)&body=Hi%2C%0A%0AI%20am%20an%20interviewer%20or%20recruiter%2C%20and%20I%20want%20to%20see%20your%20code%20base%20and%20development%20work.%20Could%20you%20please%20grant%20me%20access%3F%0A%0ARegards%2C%0A%5BYour%20Name%5D%0A%0AThank%20you."}>ankurjaiswalofficial@gmail.com</Link>
+              <Link target="_blank" href={"mailto:ankurjaiswalofficial@gmail.com"}>ankurjaiswalofficial@gmail.com</Link>
             </code>
             .
           </li>
@@ -29,20 +29,20 @@ export default function Home() {
         <div className="flex gap-4 items-start flex-col">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 font-bold"
-            href="https://mail.google.com/mail/?view=cm&fs=1&to=&su=Request%20for%20Codebase%20Access%20-%20(Whatsapp)&body=Hi%2C%0A%0AI%20am%20an%20interviewer%20or%20recruiter%2C%20and%20I%20want%20to%20see%20your%20code%20base%20and%20development%20work.%20Could%20you%20please%20grant%20me%20access%3F%0A%0ARegards%2C%0A%5BYour%20Name%5D%0A%0AThank%20you."
+            href="https://mail.google.com/mail/?view=cm&fs=1&to=ankurjaiswalofficial@gmail.com&su=Request%20for%20Codebase%20Access%20-%20(Whatsapp)&body=Hi%2C%0A%0AI%20am%20an%20interviewer%20or%20recruiter%2C%20and%20I%20want%20to%20see%20your%20code%20base%20and%20development%20work.%20Could%20you%20please%20grant%20me%20access%3F%0A%0ARegards%2C%0A%5BYour%20Name%5D%0A%5BCompany%20Name%5D%0A%0AThank%20you."
             target="_blank"
             rel="noopener noreferrer"
           >
             Request Code Access
           </a>
-          <a
+          {/* <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44 font-bold"
             href={"#"}
             target="_blank"
             rel="noopener noreferrer"
           >
             Read terms and conditions
-          </a>
+          </a> */}
         </div>
       </main>
     </div>
